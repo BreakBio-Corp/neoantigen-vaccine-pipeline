77fe960c9d74ebfa21d02c08b643883beff16565
Emon46 <hremon331046@gmail.com>
2024-02-01 18:30:00 +0600

    update image repo

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 set -ex
 
 # docker hub username
-USERNAME=hremon331046
+USERNAME=359343221949.dkr.ecr.us-east-2.amazonaws.com
 IMAGE_TAG=d0.4.2-mhc2-flexible-cutoff-v1.0.2
 # image name
 IMAGE=neoantigen-vaccine-pipeline
